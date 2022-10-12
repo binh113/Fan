f58a839f69c2743bd89b0eb382b572435fe5e223
binh113 <quocbinh230597@gmail.com>
2022-10-12 11:02:17 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(10, false, 10, "red");
+        Fan fan2 = new Fan(5, true, 5, "green");
+
+        fan1.toString();
+        fan2.toString();
+    }
+}
